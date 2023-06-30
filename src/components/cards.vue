6d24b75e432c073e2f3045706768300f47746d9e
Rortice <umiumi17510@gmail.com>
2023-06-30 18:58:57 +0900

    scriptが間違ってたのかも

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,32 +1,38 @@
 <script setup lang="ts">
-import { toRefs } from "vue";
-import { playerStore } from "@/main";
-import { storeToRefs } from "pinia";
+import type { Card } from "@/types";
 
-const { player, sumCards } = storeToRefs(playerStore);
-const { field } = toRefs(player.value);
+defineProps<{
+  cards: Card[];
+}>();
+
+const emit = defineEmits(["sendText"]);
+
+const sendTest = (index: number) => {
+  emit("sendText", index);
+};
 
 </script>
 
 <template>
-  <div v-for="(card, index) in cards" :key="card.id">
-    <button class=img-button @click="sendTest(index)">
-      <img src="../../src/assets/card1.png">
-      <h5 class="card-name">{{ card.name }}</h5>
-      <p class="card-ID">{{ card.id }}</p>
-      <p class="card-company">{{ card.company }}</p>
-      <p class="card-hungry">{{ card.waste + card.hungry }}</p>
-      <div v-if="card.pow">
-        <p class="card-attribute">{{ card.pow }}</p>
-      </div>
-      <div v-if="card.def">
-        <p class="card-attribute">{{ card.def }}</p>
-      </div>
-      <div v-if="card.tech">
-        <p class="card-attribute">{{ card.tech }}</p>
-      </div>
-    </button>
-
+  <div>
+    <div v-for="(card, index) in cards" :key="card.id">
+      <button class=img-button @click="sendTest(index)">
+        <img src="../../src/assets/card1.png">
+          <h5 class="card-name">{{ card.name }}</h5>
+          <p class="card-ID">{{ card.id }}</p>
+          <p class="card-company">{{ card.company }}</p>
+          <p class="card-hungry">{{ card.waste + card.hungry }}</p>
+          <div v-if="card.pow">
+            <p class="card-attribute">{{ card.pow }}</p>
+          </div>
+          <div v-if="card.def">
+            <p class="card-attribute">{{ card.def }}</p>
+          </div>
+          <div v-if="card.tech">
+            <p class="card-attribute">{{ card.tech }}</p>
+          </div>
+      </button>
+    </div>
   </div>
 </template>
 
