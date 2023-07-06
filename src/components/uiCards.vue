1e8fd4f6bd40352e141aa727dc89dc48723b285b
Rortice <umiumi17510@gmail.com>
2023-07-06 10:50:02 +0900

    styleの追加

--- src/components/uiCards.vue
+++ src/components/uiCards.vue
@@ -15,18 +15,18 @@ const { field } = toRefs(player.value);
         <div class="card">
           <img src="@/assets/card1.png">
           <div class="cardText w-30 h-30 p-4 flex flex-col justify-center items-center">
-            <h5 class="text-bold">name:{{ card.name }}</h5>
-            <p class="text-gray-600">ID:{{ card.id }}</p>
-            <p class="text-gray-600">📊🚬:{{ card.company }}</p>
-            <p class="text-gray-600">{{ "🍃:" + card.waste + "🍖: " + card.hungry + "🦶: " + card.priority }}</p>
+            <h5 class="card-name">name:{{ card.name }}</h5>
+            <p class="card-id">ID:{{ card.id }}</p>
+            <p class="card-company">📊🚬:{{ card.company }}</p>
+            <p class="card-hungry">{{card.waste + card.hungry + card.priority }}</p>
             <div v-if="card.atk">
-              <p class="text-gray-600">{{ "⚔:" + card.atk }}</p>
+              <p class="card-Characteristic">{{card.atk }}</p>
             </div>
             <div v-if="card.def">
-              <p class="text-gray-600">{{ "🛡:" + card.def }}</p>
+              <p class="card-Characteristic">{{card.def }}</p>
             </div>
             <div v-if="card.tech">
-              <p class="text-gray-600">{{ "🏹:" + card.tech }}</p>
+              <p class="card-Characteristic">{{card.tech }}</p>
             </div>
           </div>
         </div>
@@ -35,3 +35,35 @@ const { field } = toRefs(player.value);
     {{ sumCards }}
   </div>
 </template>
+
+<style>
+.card-name{
+  text-align: center;
+}
+
+.card-id{
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+
+.card-company{
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.card-hungry{
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.card-Characteristic{
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
